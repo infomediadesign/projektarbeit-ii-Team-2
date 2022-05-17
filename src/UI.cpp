e67bbf94e77a3410a8ef315400e5887b236eb5c3
Tranor <98488787+TranorVespucci@users.noreply.github.com>
2022-05-17 11:33:36 +0200

    And another Issue fixed. Drawing Text issue, because I am stupid

--- src/UI.cpp
+++ src/UI.cpp
@@ -3,8 +3,8 @@
 void Game::UI::Draw() {
   DrawText("***********************Controlls**************************", 10, 0, 10, BLACK);
   DrawText("*-> Move with W,A,S,D", 10, 10, 10, BLACK); // x, y , size of text
-  DrawText("*-> You can move faster with holding the LEFT SHIFT button");
+  DrawText("*-> You can move faster with holding the LEFT SHIFT button", 10, 30, 10, BLACK);
   DrawText("*-> Exit game with ESC", 10, 20, 10, BLACK);
-  DrawText("*-> toggle between windowed mode and fullscreen mode press F", 10, 30, 10, BLACK);
-  DrawText("*********************************************************", 10, 40, 10, BLACK);
+  DrawText("*-> toggle between windowed mode and fullscreen mode press F", 10, 40, 10, BLACK);
+  DrawText("*********************************************************", 10, 50, 10, BLACK);
 }
