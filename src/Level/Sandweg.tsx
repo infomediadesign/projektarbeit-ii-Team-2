af1f19e4cc6f94fd8d2fbc8e3dbebfc8e5d1b14a
THEGRANDNICK <nicovogt56@gmail.com>
2022-07-06 17:08:18 +0200

    reorganizing

--- src/Level/Sandweg.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Sandweg " tilewidth="32" tileheight="32" tilecount="7" columns="7">
- <image source="../Asprite PA2/Zeitzonen/Ägypten/Phyramide/Final Assets/FinalFinal/SandWEG_Purrfect-Sheet.png" trans="ff00ff" width="224" height="32"/>
-</tileset>
