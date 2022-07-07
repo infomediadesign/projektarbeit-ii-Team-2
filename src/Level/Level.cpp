30e699d774c450074f969ebd12ffc6f617e2eef3
Tranor <98488787+TranorVespucci@users.noreply.github.com>
2022-07-07 10:42:08 +0200

    Functional Combat System. Manipulating Turnnumber yet to be done.

--- src/Level/Level.cpp
+++ src/Level/Level.cpp
@@ -7,55 +7,73 @@ int framescounter = 0;
 
 void Game::Level::combat()
 {
-//Setting an Input to prevent the Player from attacking, while the countdown runs
+  //Setting an Input to prevent the Player from attacking, while the countdown runs
   bool input = true;
 
-//Combat init
+  //Combat init
 
-//If the Timer is true, the seconds will Run and the Player can´t press any buttons, while the Enemy Attacks
-if (timer == true)
-{
-  framescounter++;
-  input = false;
-}
+  //If the Timer is true, the seconds will Run and the Player can´t press any buttons, while the Enemy Attacks
+  if (timer == true) {
+    framescounter++;
+    input = false;
+  }
 
-//If 2 Seconds has Passed, the Player will receive Damage from the Enemy and the timer will be stopped, the Player can
-//then attack again
-      if (((framescounter/120)%2) == 1)
-      {
-        player->getDamage(enemy->attack());
-        framescounter = 0;
-        timer = false;
-        input = true;
-      }
-//Player Attack
-      if (input == true) {
-          if (IsKeyPressed(KEY_R)) {
-            enemy->getDamage(player->attack());
-            timer = true;
-        }
-      }
-//Setting up the Turnnumber
-			if (IsKeyPressed(KEY_Z))
-			{
-				player->set_turnnumb(GetRandomValue(0, 100));
-				enemy->set_turnnumb(GetRandomValue(0, 100));
-			}
-    
-	 if (enemy->getLives() <= 0) 
-	{
-      //delete enemy;
-      //delete player;
-     DrawText("YOU WON!", 600, 320, 20, BLACK);
-     timer = false;
-    } 
-	else if (player->getLives() <= 0) 
-	{
-     // delete enemy;
-     // delete player;
-      DrawText("YOU LOST!", 600, 320, 20, BLACK);
-      timer = false;
+  //If 2 Seconds has Passed, the Player will receive Damage from the Enemy and the timer will be stopped, the Player can
+  //then attack again
+  if (((framescounter/120)%2) == 1) {
+    //player->getDamage(enemy->attack());
+    framescounter = 0;
+
+    if (player->get_turnnumb() > enemy->get_turnnumb()) {
+      player->getDamage(enemy->attack());
     }
+
+    if (enemy->get_turnnumb() > player->get_turnnumb()) {
+      enemy->getDamage(player->attack());
+    }
+
+    timer = false;
+    input = true;
+  }
+  //Player Attack
+  if (input == true) {
+    if (IsKeyPressed(KEY_R)) {
+      //enemy->getDamage(player->attack());
+      player->set_turnnumb(GetRandomValue(0, 100));
+      enemy->set_turnnumb(GetRandomValue(0, 100));
+
+      if (player->get_turnnumb() > enemy->get_turnnumb()) {
+        enemy->getDamage(player->attack());
+      }
+
+      if (enemy->get_turnnumb() > player->get_turnnumb()) {
+        player->getDamage(enemy->attack());
+      }
+
+      timer = true;
+    }
+  }
+  //Setting up the Turnnumber
+  /*if (IsKeyPressed(KEY_Z))
+  {
+    player->set_turnnumb(GetRandomValue(0, 100));
+    enemy->set_turnnumb(GetRandomValue(0, 100));
+  }*/
+
+  if (enemy->getLives() <= 0)
+  {
+    //delete enemy;
+    //delete player;
+    DrawText("YOU WON!", 600, 320, 20, BLACK);
+    timer = false;
+  }
+  else if (player->getLives() <= 0)
+  {
+    // delete enemy;
+    // delete player;
+    DrawText("YOU LOST!", 600, 320, 20, BLACK);
+    timer = false;
+  }
 }
 
 void Game::Level::Screeninit() {
@@ -73,31 +91,31 @@ void Game::Level::ScreenDraw() {
 
   case GameScreen::OVERWORLD: DrawTexture(Map, 0, 0, WHITE); break;
 
-		case GameScreen::COMBAT: 
+  case GameScreen::COMBAT:
 
-		//DrawTexture(Battlescreen, 410, 320, WHITE);
-		//Draw Player
-		DrawRectangleRec(player->set_rec(), BLUE);
+    //DrawTexture(Battlescreen, 410, 320, WHITE);
+    //Draw Player
+    DrawRectangleRec(player->set_rec(), BLUE);
     //Timer
     DrawText(TextFormat("Time: %i", framescounter), player->set_rec().x + 100, player->set_rec().y, 20, BLUE);
 
-		DrawText(TextFormat("Speed: %i", player->get_turnnumb()), player->set_rec().x, player->set_rec().y - 90, 20, BLUE);
-		DrawText(TextFormat("HP: %i", player->getLives()), player->set_rec().x, player->set_rec().y - 70, 20, BLUE);
-		DrawText(TextFormat("STR: %i", player->getStrength()), player->set_rec().x, player->set_rec().y - 50, 20, BLUE);
-		DrawText(TextFormat("DEF: %i", player->getArmor()), player->set_rec().x, player->set_rec().y - 30, 20, BLUE);
-		//Draw enemy
-		DrawText(TextFormat("Speed: %i", enemy->get_turnnumb()), enemy->set_rec().x, enemy->set_rec().y - 90, 20, RED);
-		DrawRectangleRec(enemy->set_rec(), RED);
-		DrawText(TextFormat("HP: %i", enemy->getLives()), enemy->set_rec().x, enemy->set_rec().y - 70, 20, RED);
-		DrawText(TextFormat("STR: %i", enemy->getStrength()), enemy->set_rec().x, enemy->set_rec().y - 50, 20, RED);
-		DrawText(TextFormat("DEF: %i", enemy->getArmor()), enemy->set_rec().x, enemy->set_rec().y - 30, 20, RED);
+    DrawText(TextFormat("Speed: %i", player->get_turnnumb()), player->set_rec().x, player->set_rec().y - 90, 20, BLUE);
+    DrawText(TextFormat("HP: %i", player->getLives()), player->set_rec().x, player->set_rec().y - 70, 20, BLUE);
+    DrawText(TextFormat("STR: %i", player->getStrength()), player->set_rec().x, player->set_rec().y - 50, 20, BLUE);
+    DrawText(TextFormat("DEF: %i", player->getArmor()), player->set_rec().x, player->set_rec().y - 30, 20, BLUE);
+    //Draw enemy
+    DrawText(TextFormat("Speed: %i", enemy->get_turnnumb()), enemy->set_rec().x, enemy->set_rec().y - 90, 20, RED);
+    DrawRectangleRec(enemy->set_rec(), RED);
+    DrawText(TextFormat("HP: %i", enemy->getLives()), enemy->set_rec().x, enemy->set_rec().y - 70, 20, RED);
+    DrawText(TextFormat("STR: %i", enemy->getStrength()), enemy->set_rec().x, enemy->set_rec().y - 50, 20, RED);
+    DrawText(TextFormat("DEF: %i", enemy->getArmor()), enemy->set_rec().x, enemy->set_rec().y - 30, 20, RED);
 
-		combat();
+    combat();
 
-		//DrawText("COMBATSCREEN FOR CUSTODIA", 500, 320, 20, GRAY);
-		//DrawText("PRESS ENTER TO RETURN", 500, 340, 20, GRAY);
-      break;
-	}
+    //DrawText("COMBATSCREEN FOR CUSTODIA", 500, 320, 20, GRAY);
+    //DrawText("PRESS ENTER TO RETURN", 500, 340, 20, GRAY);
+    break;
+  }
 }
 
 //void Game::Level::Draw() { DrawTexture(Map, 0, 0, WHITE); }
